a531bc54577c10e9fbd20b3229559f95cb2ddd82
Xin <ilcb112045@gmail.com>
2019-08-10 23:01:16 +0800

    add readme and modify

---
+++ README.md
@@ -0,0 +1,7 @@
+getImage.py
+You can use it to download coco images from coco json file.
+
+Sometimes you will get ConnectionResetError because of the remote server resctriction.
+If this program is blocked, it will "ignore that image" and sleep 1 min and then continute to run.
+So, after finishing the program, maybe you will miss some images.
+You can run it and redownload. (Images you've downloaded won't be downloaded again.)
--- getImage.py
+++ getImage.py
@@ -4,14 +4,15 @@ import time
 import os.path
 import csv
 
-jsonPath = 'annotations'
-imgdirpath = 'downloaded_images'
+#======================================================The area you can modify
+jsonPath = 'annotations'                #The directory path where coco json file is
+imgdirpath = 'downloaded_images/train2017'        #The directory path where the downloaded images are stored
 
-jsonFile = 'instances_train2017.json'
-imgDir = "train2017"
+jsonFile = 'instances_train2017.json'       #The json file downloaded from coco
 
-categories = ['person']
 
+categories = ['person']             #Specify the categories you want (go to coco to see what categories you can select)
+#======================================================
 
 coco = COCO(jsonPath+"/"+jsonFile)
 cats = coco.loadCats(coco.getCatIds())
@@ -27,7 +28,7 @@ count_downloaded = 0
 for im in images:
     imgName = im['file_name']
     try:
-        if os.path.isfile(imgdirpath +"/" + imgDir + "/" + im['file_name']):
+        if os.path.isfile(imgdirpath+ "/" + im['file_name']):
             count_downloaded = count_downloaded+1
             name = im['file_name'].split(".")[0]
             print("Image : ",name," was already downloaded.   Pass")
@@ -36,13 +37,12 @@ for im in images:
 
         img_data = requests.get(im['coco_url']).content
         count_downloaded  = count_downloaded +1
-        with open(imgdirpath +"/" + imgDir + "/" + im['file_name'], 'wb') as handler:
+        with open(imgdirpath+ "/" + im['file_name'], 'wb') as handler:
             handler.write(img_data)
 
         print("===================      The number of downloaded images : {}".format(str(count_downloaded)),"     ===================")
 
-        if(count_downloaded==10):
-            raise ConnectionResetError
+
     except (requests.exceptions.RequestException,ConnectionResetError) as err:
         count_blocked = count_blocked+1
         print(err)
@@ -51,4 +51,4 @@ for im in images:
         continue
 
 print("=========    Downloaded Images : ",count_downloaded,"    =========")
-print("=========    Blocked Images : ",count_blocked,"  =========")
+print("=========    Blocked Images : ",count_blocked,"  =========")#p
